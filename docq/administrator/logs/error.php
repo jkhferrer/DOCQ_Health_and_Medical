d15596f329d35f7be097551c6c75e83f893ac76c
Ken Ferrer <jkhferrer@gmail.com>
2017-09-04 22:12:25 +0800

    Docq 1.0 has some functionality issue in calendar and appointment

---
+++ docq/administrator/logs/error.php
@@ -0,0 +1,10 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2017-08-24 14:08:17 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2017-08-24T14:08:17+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2017-08-25T04:27:40+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2017-08-25T04:28:00+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2017-08-28T14:05:51+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
